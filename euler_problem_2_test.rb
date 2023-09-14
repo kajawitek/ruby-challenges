912b5b41de104f2faaa54c6d1a29eedd97a00763
Kaja Witek <kaja.wasiolek@gmail.com>
2023-09-14 22:17:24 +0200

    solved p2

---
+++ euler_problem_2_test.rb
@@ -0,0 +1,38 @@
+# https://projecteuler.net/problem=2
+# `gem install minitest` to install minitest
+# `ruby euler_problem_2_test.rb` to run the test
+#
+require 'minitest/autorun'
+
+class EvenFibonacciNumbersTest < Minitest::Test
+  def test_sum_of_even_fibonacci_numbers_below_100
+    assert_equal 44, EvenFibonacciNumbers.new.sum_of_even_fibonacci_numbers_below(100)
+  end
+
+  def test_sum_of_even_fibonacci_numbers_below_4000000
+    assert_equal 4613732, EvenFibonacciNumbers.new.sum_of_even_fibonacci_numbers_below(4000000)
+  end
+end
+
+class EvenFibonacciNumbers
+  def sum_of_even_fibonacci_numbers_below(limit)
+      result = 0
+
+      prev_prev_result = 0
+      prev_result = 1
+      sum = 0
+
+      return 0 if limit == 0
+      return 1 if limit == 1
+
+      (2..limit).each_with_index do |number, index|
+        result = prev_result + prev_prev_result
+        prev_prev_result = prev_result
+        prev_result = result
+
+        return sum if result >= limit
+
+        sum = sum + result if result.even?
+      end
+  end
+end
